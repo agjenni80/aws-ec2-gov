28c25a53115dfce4f29da83462c3bacfb94add23
Amanda Jennings <ajennings@hashicorp.com>
2019-04-30 11:01:17 -0400

    initial commit

---
+++ main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_version = ">= 0.11.13"
+}
+
+provider "aws" {
+  region = "${var.aws_region}"
+}
+
+resource "aws_instance" "web" {
+  ami           = "${var.ami_id}"
+  instance_type = "${var.instance_type}"
+  availability_zone = "${var.aws_region}a"
+  associate_public_ip_address = "true"
+  count =2
+  root_block_device {
+    volume_size = "${var.volume_size}"
+  }
+ 
+  tags {
+    environment = "Terraform AWS GovCloud Demo"
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,37 @@
+variable "aws_region" {
+  description = "AWS region"
+  default = "us-gov-west-1"
+}
+
+variable "ami_id" {
+  description = "ID of the AMI to provision. Default is Ubuntu 14.04 Base Image"
+  default = "ami-f63b1193"
+}
+
+variable "instance_type" {
+  description = "type of EC2 instance to provision."
+  default = "t2.micro"
+}
+
+variable "name" {
+  description = "name to pass to Name tag"
+  default = "ajennings"
+}
+
+variable "key_name" {
+   description = "name of the SSH key for accessing the instance"
+   default = "ajennings-key"
+  }  
+
+variable "volume_size" {
+  description = "the size of root device"
+  default = "40"
+}
+
+variable "public_key_path" {
+  default = "/Users/agjennings/.ssh/ajennings-key.pub"
+}
+
+variable "private_key_data" {
+  default =""
+}
